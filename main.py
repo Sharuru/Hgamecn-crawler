5362ad5bfd22a17ade35efe2d4659bbd1f2b833f
Mave <i@0w0.re>
2014-08-12 20:46:39 +0800

    complete Database Structure
    New: Use SQLite at current time.
    New: May change to SQLa to operate data later.

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 __author__ = 'Mave'
 
 import urllib2
+import sqlite3
+#Create Link Object
 request = urllib2.Request('http://www.hgamecn.com/htmldata/articlelist/')
+request.add_header('User-Agent', 'Macho Macho Man')
 
+#Create Database Object
+db = sqlite3.connect('Page_Record.db')
 try:
-    response = urllib2.urlopen(request)
+    response = urllib2.urlopen(request, timeout=5)
 except urllib2.URLError as e:
     if hasattr(e, 'reason'):
         print 'Sorry, I can not get the target website. (Because of me)'
@@ -14,8 +19,11 @@ except urllib2.URLError as e:
         print 'Error Code: ', e.code
 else:
     print 'All right, I can get the target website.'
+    print 'Code: ', response.code
     page = response.read()
     print page
 
 
-
+def page_switcher(begin_page, end_page):
+    for current_page in range(begin_page, end_page+1):
+        print page

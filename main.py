a85c5beb7baf7f966e0c39cc74c9c077b2e04563
Mave <i@0w0.re>
2014-08-13 15:52:01 +0800

    fetch game info

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ else:
     print 'Counting Record Pages...',
     page_finder_head = game_content.find('<div class="hgc_pages">1/')
     page_finder_tail = game_content.find('<span class="hgc_pages_con">')
-    total_page = int(game_content[page_finder_head+25:page_finder_tail-13])
+    total_page = int(game_content[page_finder_head + 25: page_finder_tail - 13])
     print total_page
     print 'Calculate Finished.'
     print 'Now, I am going to process data in page {number}.'.format(number=current_page)
@@ -57,9 +57,38 @@ else:
     game_publisher = page_content.find_all('span', 'maker')
     game_publish_date = page_content.find_all('span', 'date')
     game_tags = page_content.find_all('div', 'tag')
-    for something in range(0, 20):
-        print game_title[something]
-        #print game_publisher[something]
-        #print game_publish_date[something]
-        #print game_tags[something]
+    for records in range(0, game_number_in_current_page):
+        game_title_str = str(game_title[records])
+        game_publisher_str = str(game_publisher[records])
+        game_publish_date_str = str(game_publish_date[records])
+        game_tags_str = str(game_tags[records])
 
+        game_title_finder_head = game_title_str.find('target="_blank">')
+        game_title_finder_tail = game_title_str.find('</a></div>')
+        game_title[records] = game_title_str[game_title_finder_head + 16: game_title_finder_tail - 4]
+
+        game_publisher_finder_head = game_publisher_str.find('>', 32)
+        game_publisher_finder_tail = game_publisher_str.find('</a></span>')
+        game_publisher[records] = game_publisher_str[game_publisher_finder_head + 1: game_publisher_finder_tail]
+
+        game_publish_date_finder_head = game_publish_date_str.find('>', 36)
+        game_publish_date_finder_tail = game_publish_date_str.find('</a></span>')
+        game_publish_date[records] = game_publish_date_str[game_publish_date_finder_head + 1: game_publish_date_finder_tail]
+
+        game_tags_finder_head = 0
+        game_tags_finder_tail = 0
+        head = 30
+        for tag_number in range(0, 5):
+            #if game_tags_finder_head != len(game_tags_str):
+                game_tags_finder_head = game_tags_str.find('>', head)
+                game_tags_finder_tail = game_tags_str.find('</a>', game_tags_finder_head)
+                game_tags[records] = game_tags_str[game_tags_finder_head + 1: game_tags_finder_tail]
+                head = game_tags_finder_tail
+
+
+
+
+        print game_title[records]
+        print game_publisher[records]
+        print game_publish_date[records]
+        print game_tags[records]
